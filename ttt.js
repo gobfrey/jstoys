8311faa8258b5aa5f7346b86d1086c4925a386fa
Adam Field <adam@adamfield.net>
2016-10-16 06:02:37 -0700

    change names to reflect jscript naming convention of camel case

--- ttt.js
+++ ttt.js
@@ -4,20 +4,20 @@ $(document).ready(function(){
 		//don't allow a click if...
 		if (
 			!$(this).hasClass('blank') //the cell allready has an X or O
-			|| ttt_game_is_over()
+			|| tttGameIsOver()
 		)
 		{
 			return;
 		}
 
-		var current_player = ttt_get_current_player(); //are we clicking X or O
+		var currentPlayer = tttGetCurrentPlayer(); //are we clicking X or O
 
-		ttt_set_cell_content(this, current_player); //set X or O on this cell
+		tttSetCellContent(this, currentPlayer); //set X or O on this cell
 
-		var win_line = ttt_detect_win_line(current_player); //did the current player just win?
-		if (win_line)
+		var winLine = tttDetectWinLine(currentPlayer); //did the current player just win?
+		if (winLine)
 		{
-			ttt_win(current_player, win_line); //update interface to reflect win
+			tttWin(currentPlayer, winLine); //update interface to reflect win
 		}
 
 	});
@@ -25,28 +25,28 @@ $(document).ready(function(){
 });
 
 //return the line class of a win for a nought or a cross player
-function ttt_detect_win_line(current_player)
+function tttDetectWinLine(currentPlayer)
 {
 	//check to see if anyone has won by checking all possible lines of 3
-	var win_lines = ['row-1','row-2','row-3','col-1','col-2','col-3','slash','backslash'];
+	var winLines = ['row-1','row-2','row-3','col-1','col-2','col-3','slash','backslash'];
 
 	//select each line of three that has this click's piece
-	for (var i = 0; i < win_lines.length; i++)
+	for (var i = 0; i < winLines.length; i++)
 	{
 		//e.g. .cross.row-1 to see how many crosses are in row 1
-		var class_selector = '.' + current_player + '.' + win_lines[i];
+		var classSelector = '.' + currentPlayer + '.' + winLines[i];
 
 		//are there three Xs or Os in this line
-		if ($(class_selector).length == 3)
+		if ($(classSelector).length == 3)
 		{
-			return win_lines[i];
+			return winLines[i];
 		}
 	}
 	return null;
 }
 
 //Whose turn is it?
-function ttt_get_current_player()
+function tttGetCurrentPlayer()
 {
 	//if there are an even number of empty cells, it's an O (let's take turns)
 	if( $('.blank').length % 2 == 0)
@@ -57,29 +57,29 @@ function ttt_get_current_player()
 }
 
 //set the background image of the cell, add nought or cross class, remove blank class
-function ttt_set_cell_content(cell, current_player)
+function tttSetCellContent(cell, currentPlayer)
 {
 
 	//assume it's an X
-	var bg_img = 'url(img/crosses.png)';
+	var bgImg = 'url(img/crosses.png)';
 
-	if( current_player == 'nought')
+	if( currentPlayer == 'nought')
 	{
-		bg_img = 'url(img/noughts.png)';
+		bgImg = 'url(img/noughts.png)';
 	}
 
 	//set new background image
 	$(cell).css({
-			'background' : bg_img,
+			'background' : bgImg,
 			'background-position' : $(cell).css('background-position') //preserve position
 			});
 
 	//set new class
 	$(cell).removeClass('blank');
-	$(cell).addClass(current_player);
+	$(cell).addClass(currentPlayer);
 }
 
-function ttt_game_is_over()
+function tttGameIsOver()
 {
 	if (
 		$('.win-line').length //the came has already been won
@@ -92,16 +92,16 @@ function ttt_game_is_over()
 }
 
 //update the interface to reflect game over.
-function ttt_win(current_player, win_line_class)
+function tttWin(currentPlayer, winLineClass)
 {
 	//make this the winning line
-	$('.' + win_line_class).addClass('win-line');
+	$('.' + winLineClass).addClass('win-line');
 
-	var win_message = 'X Wins';
-	if (current_player == 'nought')
+	var winMessage = 'X Wins';
+	if (currentPlayer == 'nought')
 	{
-		win_message = 'O Wins';
+		winMessage = 'O Wins';
 	}
 
-	$('#ttt-container').after('<div id="win-status">' + win_message + '</div>');
+	$('#ttt-container').after('<div id="win-status">' + winMessage + '</div>');
 }
